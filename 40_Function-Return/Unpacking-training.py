158a75933a1c7fde8e2cab06dae0a4dbc81841f3
Ahmad Haymour <haimour22@gmail.com>
2022-11-20 06:59:42 +0100

    Training

---
+++ 40_Function-Return/Unpacking-training.py
@@ -0,0 +1,26 @@
+#------------------------------------------------------#
+# -- Function Packing, Unpacking Arguments Training -- #
+#------------------------------------------------------#
+
+myTuple = ("HTML", "CSS", "JS")
+mySkills = {
+    "Python": "50%",
+    "JAVA": "10%",
+    "mySQL": "80%"
+}
+
+def show_skills(user, *skills, **skillsAndProgress) :
+    print("Hello {}\nSkills without Progress is =>".format(user))
+
+    for skill in skills :
+        print('*- {}'.format(skill))
+
+    print("Skills with Progress is =>")
+
+    for skill_key, skill_value in skillsAndProgress.items() :
+        print('*- {} => {}'.format(skill_key, skill_value))
+
+
+show_skills('Ahmad', "HTML", "CSS", "JS", Python='60%', JAVA = "20%")
+
+show_skills("Lara", *myTuple, **mySkills)
