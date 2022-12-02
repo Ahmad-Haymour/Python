cceb141ba50460138c07553b40850163e1ff6c94
Ahmad Haymour <haimour22@gmail.com>
2022-12-03 00:44:00 +0100

    OOP Class Attributes and Methods Part 2

--- 57_OOP_Object-Oriented-Programming/02_Instance-Attributes-Methods.py
+++ 57_OOP_Object-Oriented-Programming/02_Instance-Attributes-Methods.py
@@ -1,7 +1,56 @@
-# -------------------------------------------------------------
-# -- Object Oriented Programming => Class Syntax and Info --
-# -------------------------------------------------------------
-# [01] Class is the Blueprint or Constructor of the Object
-# [02] Class Instantiate Means Crete Instance of a Class
-# [03] Instance => Object Created from Class and have their Methods and Attributes
-# [04] Class defined with Keyword class
+# --------------------------------------------------------------------
+# -- Object Oriented Programming => Instance Attributes and Methods --
+# --------------------------------------------------------------------
+# Self: Point to Instance Created from Class
+# Instance Attributes: Instance Attributes Defined Inside the Constructor
+# --------------------------------------------------------------------
+# Instance Methods: Take Self Parameter Which Point to Instance Created from Class
+# Instance Methods can have more than one Parameter Like any Function
+# Instance Methods can freely access Attributes and Methods on the same Object
+# Instance Methods can access the Class Itself
+# --------------------------------------------------------------------
+
+class Member:
+
+    def __init__(self, first_name, middle_name, last_name, gender):
+
+        self.fname = first_name
+        self.mname = middle_name
+        self.lname = last_name
+        self.gender = gender
+
+    def full_name(self):
+
+        return f'{self.fname} {self.mname} {self.lname}'
+
+    def name_with_title(self):
+
+        if self.gender == 'Male':
+            return f'Hello Mrs {self.fname}'
+
+        elif self.gender == 'Female':
+            return f'Hello Miss {self.fname}'
+
+        else:
+            return f'Hello {self.fname}'
+
+    def get_all_info(self):
+
+        return f'{self.name_with_title()}, your Fullname is => {self.full_name()}'
+
+
+member_one = Member('Ahmad', 'Mohammed', 'Rami', 'Male')
+member_two = Member('Osama', 'Mohammed', 'Alsayed', 'Male')
+member_three = Member('Lara', 'Philip', 'Möller', 'Female')
+
+print(dir(member_one))
+
+print(member_one.fname, member_one.mname, member_one.lname)
+print(member_two.fname, member_two.lname )
+print(member_three.fname)
+
+print(member_one.full_name())
+print(member_one.name_with_title())
+
+print(member_one.get_all_info())
+print(member_three.get_all_info())
