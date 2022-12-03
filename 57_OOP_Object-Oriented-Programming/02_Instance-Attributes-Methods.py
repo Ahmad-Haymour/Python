a099223f737bd663be32db5a3ed6e1644a517d37
Ahmad Haymour <haimour22@gmail.com>
2022-12-03 01:08:25 +0100

    OOP Class Attributes and Methods Part 2

--- 57_OOP_Object-Oriented-Programming/02_Instance-Attributes-Methods.py
+++ 57_OOP_Object-Oriented-Programming/02_Instance-Attributes-Methods.py
@@ -12,12 +12,18 @@
 
 class Member:
 
+    not_allowed_names = ['Hell', 'Shit', 'Baloot']
+
+    users_count = 0
+
     def __init__(self, first_name, middle_name, last_name, gender):
 
         self.fname = first_name
         self.mname = middle_name
         self.lname = last_name
         self.gender = gender
+
+        Member.users_count += 1
 
     def full_name(self):
 
@@ -36,12 +42,25 @@ class Member:
 
     def get_all_info(self):
 
+        if self.fname in Member.not_allowed_names:
+            raise ValueError('Name not allowed')
+
         return f'{self.name_with_title()}, your Fullname is => {self.full_name()}'
 
+    def delete_user(self):
+        Member.users_count -= 1
+        return f'User {self.fname} deleted'
 
+
+print(Member.users_count)
 member_one = Member('Ahmad', 'Mohammed', 'Rami', 'Male')
 member_two = Member('Osama', 'Mohammed', 'Alsayed', 'Male')
 member_three = Member('Lara', 'Philip', 'Möller', 'Female')
+member_four = Member('Hell', 'Shit', 'Blabla', 'Male')
+
+print(Member.users_count)
+print(member_four.delete_user())
+print(Member.users_count)
 
 print(dir(member_one))
 
@@ -54,3 +73,5 @@ print(member_one.name_with_title())
 
 print(member_one.get_all_info())
 print(member_three.get_all_info())
+
+# print(member_four.get_all_info())
