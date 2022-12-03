99821a801c9d411e34937db301e0aa84ca897b96
Ahmad Haymour <haimour22@gmail.com>
2022-12-03 20:33:19 +0100

    OOP Class Inheritance 1

---
+++ 57_OOP_Object-Oriented-Programming/05_Inheritance.py
@@ -0,0 +1,45 @@
+# --------------------------------------------------
+# -- Object Oriented Programming => Inheritance --
+# --------------------------------------------------
+
+class Food:     # Base Class
+
+    def __init__(self, name, price):
+
+        self.name = name
+        self.price = price
+        print(f'{self.name} is created from Base Class')
+
+    def eat(self):
+
+        print('Eat Method from Base Class')
+
+#   Inherit all Food Methods in apple Class
+class Apple(Food):    # Derived Class
+
+# This init Finction will Override Parameters
+
+    def __init__(self, name, price, amount):
+
+        # Food.__init__(self, name)       # Crete Instance from Base Class
+        super().__init__(name, price)            # Crete Instance from Base Class(New Way)
+
+        self.amount = amount
+
+        print(f'{self.name} is created from Derived Class, and Price is: {self.price}, and Amount is: => {self.amount}')
+
+    def get_from_tree(self):
+
+        print(f'Get from tree from Derived Class => {self.price + 20}')
+
+
+# food_one = Food('Orange', 20)
+
+food_two = Apple('Pizza', 150, 500)
+food_two.eat()
+print(food_two.name)
+food_two.get_from_tree()
+
+print('><'*40)
+
+
