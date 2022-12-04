66a4d5ce60abcae0c51d1f0e3dfdd189d96b15e6
Ahmad Haymour <haimour22@gmail.com>
2022-12-04 12:38:40 +0100

    DataBase Intro

---
+++ 58_DataBase/01_Intro.py
@@ -0,0 +1,12 @@
+# --------------------------
+# -- Databases => Intro --
+# --------------------------
+# - Database is a place where we can store Data
+# - Database organized int tables (Users, Categories)
+# - Tables has columns (ID, Username, Password)
+# - There's many Types of Databases ( MongoDB, MySQL, SQLite )
+# - SQL => stand for structured Query Language
+# - SQLite => can run in Memory or in a single File
+# - You can browse with https://sqlitebrowser.org
+# - Data inside Database has Types (Text, Integer, Date)
+# ---------------------------------------------------------
