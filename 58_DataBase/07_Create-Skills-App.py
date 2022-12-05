4f5cc52e3e6989de24d0c5e732d314ba28a52b00
Ahmad Haymour <haimour22@gmail.com>
2022-12-05 14:53:19 +0100

    Skills App Part 2

--- 58_DataBase/07_Create-Skills-App.py
+++ 58_DataBase/07_Create-Skills-App.py
@@ -1,6 +1,9 @@
 # -----------------------------------------------
 # -- Databases => SQLite => Create Skills App --
 # -----------------------------------------------
+
+import sqlite3
+
 # Input Big Message
 input_message = """
 What do you want to do ?
@@ -12,6 +15,18 @@ What do you want to do ?
 Choose Option:
 """
 
+db = sqlite3.connect('app.db')
+
+cr = db.cursor()
+
+
+def commit_close():
+
+    db.commit()
+    db.close()
+    print('Connection to Database is Closed')
+
+
 user_input = input(input_message).strip().lower()
 
 print(user_input)
@@ -19,14 +34,25 @@ print(user_input)
 # Command List
 command_list = ["s", "a", "d", "u", "q"]
 
-def show_skill():
-    print('Show Skill')
+user_id = 2
+
+
+# def show_skill():
+#     print('Show Skill')
+
+
 def add_new_skill():
-    print(f'Add New Skill')
+    sk = input('Add New Skill: ')
+    prog = input("Add Progress: ")
+    cr.execute(f'insert into skills (name, progress, user_id) values("{sk}", "{prog}", {user_id} )')
+    commit_close()
 
 
 def delete_skill():
     print(f'Delete Skill')
+    sk = input('Write Skill Name: ')
+    cr.execute(f'delete from skills where name = "{sk}" and user_id = {user_id} ')
+    commit_close()
 
 
 def update_skill():
@@ -43,7 +69,8 @@ if user_input in command_list:
 
     if user_input == "s":
 
-        show_skill()
+        # show_skill()
+        pass
 
     elif user_input == "a":
 
